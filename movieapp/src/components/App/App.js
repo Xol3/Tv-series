f87ed340acf2badeefcbfc80abb3bb6ed2f85190
Xol3 <xoalxiyetl@gmail.com>
2018-08-28 23:01:55 -0500

    got the life search to work

--- movieapp/src/components/App/App.js
+++ movieapp/src/components/App/App.js
@@ -1,19 +1,13 @@
 //why component below in {}
 import React, { Component } from 'react';
-import Intro from '../Intro/Index.js'
+import Intro from '../Intro/Index.js';
+import Series from '../../containers/series/index.js'
 import './App.css';
 import 'whatwg-fetch';
 
 //Main app component
 class App extends Component {
-  state = {
-    series: [] //state
-  }
-  componentDidMount(){
-    fetch('http://api.tvmaze.com/search/shows?q=Vikings')//need to resolved
-      .then(response => response.json()) // need to parse the results and parse the responce
-      .then(json => this.setState({series: json}))
-  }
+
   render() {
     return (
       <div className="App">
@@ -21,7 +15,7 @@ class App extends Component {
           <h1 className="App-title">Panchos TV series</h1>
         </header>
       <Intro message = "Aqui estan las series chingonas que le gustan a pancho" />
-      The length of the series array - {this.state.series.length}
+      <Series />
       </div>
     );
   }
--- movieapp/src/containers/series/index.js
+++ movieapp/src/containers/series/index.js
@@ -1,1 +1,31 @@
-import React,{component} from react;
+import React,{Component} from 'react';
+import SeriesList from '../../components/SeriesList'
+
+class Series extends Component {
+  state = {
+    series: [] //state
+  }
+// `` allows to hot manipulate url  e will get inserted
+  onSeriesInputChange = e => {
+    fetch(`http://api.tvmaze.com/search/shows?q=${e.target.value}`)//need to resolved
+      .then(response => response.json()) // need to parse the results and parse the responce
+      .then(json => this.setState({series: json}))
+    console.log(e);
+    console.log(e.target.value);
+
+  }
+
+  render(){
+    return(
+      <div>
+      The length of the series array - {this.state.series.length}
+      <div>
+        <input type="text" onChange={this.onSeriesInputChange} />
+      </div>
+      <SeriesList list={this.state.series}/>
+      </div>
+    )
+  }
+}
+
+export default Series;
