3f167a994d7f789e21f2dcb580162752b3fe6daa
Xol3 <xoalxiyetl@gmail.com>
2018-08-28 23:16:56 -0500

    added conditional for input and error messages

--- movieapp/src/containers/series/index.js
+++ movieapp/src/containers/series/index.js
@@ -3,25 +3,42 @@ import SeriesList from '../../components/SeriesList'
 
 class Series extends Component {
   state = {
-    series: [] //state
+    series: [], //state
+    seriesName:'',
+    ifFetching: false
   }
 // `` allows to hot manipulate url  e will get inserted
   onSeriesInputChange = e => {
+    this.setState({ seriesName: e.target.value, isFetching: true}) //allows us to see  the e target value
     fetch(`http://api.tvmaze.com/search/shows?q=${e.target.value}`)//need to resolved
       .then(response => response.json()) // need to parse the results and parse the responce
-      .then(json => this.setState({series: json}))
+      .then(json => this.setState({series: json, isFetching: false}))
     console.log(e);
     console.log(e.target.value);
 
   }
 
   render(){
+    const{ series, seriesName, isFetching} = this.state;
+
     return(
       <div>
-      The length of the series array - {this.state.series.length}
       <div>
-        <input type="text" onChange={this.onSeriesInputChange} />
+        <input
+        value={seriesName}
+        type="text"
+        onChange={this.onSeriesInputChange} />
       </div>
+      {
+        series.length === 0 && seriesName.trim() === ''
+        &&
+        <p>Orale Carnalito. Buscale en la cajita. </p>
+      }
+      {
+        series.length === 0 && seriesName.trim() !== ''
+        &&
+        <p>Nel pastel, no encontre nada</p>
+      }
       <SeriesList list={this.state.series}/>
       </div>
     )
